8125647f4e4728048b50d38dbcaf2d2dad265ae1
dani07ja <daniizquierdo2006@gmail.com>
2025-11-05 08:36:47 -0500

    restaurar  los archivos

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
 import Prc from "../pages/Prc";
 import PrcSuccess from "./pages/PrcSuccess";
 import PrincError from './pages/PrincError';
@@ -9,7 +8,6 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/Prc" element={<Prc />} />
         <Route path="/PrincError" element={<PrincError />} />
         <Route path="/PrcSuccess" element={<PrcSuccess />} />
